0c5ccea0fdf0c29c60dc769255de0556aad6d8a7
pietrolo94 <salamoumayma@gmail.com>
2023-04-26 16:45:34 +0200

    drgfgdfg

--- app4.py
+++ app4.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import csv
 import pandas as pd
+from funzioni import *
 
 #crea file csv se non esiste già
 try:
@@ -28,73 +29,6 @@ except FileExistsError:
 
     pass
 
-#funzione modifica cella bambini
-def modifica_cella(df, index, colonna, valore):
-    df.at[index, colonna] = valore
-    df.to_csv('dati_bambini.csv', index=False)
-    st.success('Valore modificato!')
-    st.write(df)
-#funzione modifica cella educatori
-def modifica_cella_ed(df, index, colonna, valore):
-    df.at[index, colonna] = valore
-    df.to_csv('educatori.csv', index=False)
-    st.success('Valore modificato!')
-# Funzione per modificare un valore nella tabella delle spese
-def modify_expense_data(row_index, new_value):
-    df = pd.read_csv('spese.csv')
-    df.at[row_index, 'Importo'] = new_value
-    df.to_csv('spese.csv', index=False)
-# Funzione per rimuovere una riga dalla tabella delle spese
-def remove_expense_data(row_index):
-    df = pd.read_csv('spese.csv')
-    df.drop(row_index, inplace=True)
-    df.to_csv('spese.csv', index=False)
-#funzione per scrivere i dati su file (spostata nella pagina Registrazione)
-def scrivi_su_file(nome, cognome, eta, sesso, classe, quota, diete, allergie, email, telefono1, telefono2, indirizzo, cap, comune, giorno1, pranzo1, giorno2, pranzo2, giorno3, pranzo3, giorno4, pranzo4, giorno5, pranzo5, giorno6, pranzo6, giorno7, pranzo7, giorno8, pranzo8, tesseramento, ritiro, parente, foto):
-    #controlla se il bambino è già presente nel file
-    with open('dati_bambini.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            if row['Nome'] == nome and row['Cognome'] == cognome:
-                st.error('Bambino già registrato')
-                return
-    #scrive il nuovo bambino nel file
-    with open('dati_bambini.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([nome, cognome, eta, sesso, classe, quota, diete, allergie, email, telefono1, telefono2, indirizzo, cap, comune, giorno1, pranzo1, giorno2, pranzo2, giorno3, pranzo3, giorno4, pranzo4, giorno5, pranzo5, giorno6, pranzo6, giorno7, pranzo7, giorno8, pranzo8, tesseramento, ritiro, parente, foto])
-
-#funzione per scrivere i dati su file per educatori
-def scrivi_su_file_ed(nome_ed, cognome_ed, ore_giorno1, ore_giorno2, ore_giorno3, ore_giorno4, ore_giorno5, ore_giorno6, ore_giorno7, ore_giorno8):
-    #controlla se l'educatore è già presente nel file
-    with open('educatori.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            if row['Nome'] == nome_ed and row['Cognome'] == cognome_ed:
-                st.error('Educatore già registrato')
-                return
-    
-    #scrive il nuovo educatore nel file
-    with open('educatori.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([nome_ed, cognome_ed, ore_giorno1, ore_giorno2, ore_giorno3, ore_giorno4, ore_giorno5, ore_giorno6, ore_giorno7, ore_giorno8])
-    
-    st.success('Dati salvati!')
-#funzione per scrivere su file spese
-def save_expense_data(tipo_spesa, importo):
-    with open("spese.csv", "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow([tipo_spesa, importo])
-    st.success("Spesa aggiunta con successo!")
-#funzione per calcolare le ore di ogni educatore
-def calcola_ore_totali(df):
-    df['Ore totali'] = df['Ore giorno1'] + df['Ore giorno2'] + df['Ore giorno3'] + df['Ore giorno4'] + df['Ore giorno5'] + df['Ore giorno6'] + df['Ore giorno7'] + df['Ore giorno8']
-    return df
-#funzione per calcolare la retribuzione di ogni educatore
-def calcola_compensi(df):
-    df['Compensi'] = (df['Ore giorno1'] + df['Ore giorno2'] + df['Ore giorno3'] + df['Ore giorno4']+ df['Ore giorno5'] + df['Ore giorno6'] + df['Ore giorno7'] + df['Ore giorno8'])*9
-    return df
-def convert_df(df):
-   return df.to_csv(index=False).encode('utf-8')
 def main():
     #Setting della pagina
     st.set_page_config(page_title="Palazola", page_icon=":star:", layout="wide")
@@ -399,12 +333,14 @@ def main():
                 ore_totali = df1['Ore totali'].sum()
                 st.write("Ore totali educatori: {}".format(round(ore_totali, 2)))
                 spese = df2['Importo'].sum()
+                costo_pranzi = int(df['Pranzo giorno1'].value_counts()+df['Pranzo giorno2'].value_counts()+df['Pranzo giorno3'].value_counts()+df['Pranzo giorno4'].value_counts()+df['Pranzo giorno5'].value_counts()+df['Pranzo giorno6'].value_counts()+df['Pranzo giorno7'].value_counts()+df['Pranzo giorno8'].value_counts())*7
+                st.write('Costo totale pranzi: {} euro'.format(round(costo_pranzi, 2)))
                 st.write('Totale spese:{} euro'.format(round(spese, 2)))
-                incasso_netto = quota_totale-compensi_totali-spese-costo_tesseramenti
+                incasso_netto = quota_totale-compensi_totali-spese-costo_tesseramenti-costo_pranzi
                 st.write('Incasso netto: {} euro'.format(round(incasso_netto, 2)))
                 df_report = pd.DataFrame({'Numero totale bambini':[num_bambini_tot],'Eta media':[eta_media], 
                                           'Quota media':[quota_media],'Incasso lordo':[quota_totale], 'Compensi educatori':[compensi_totali],
-                                          'Ore totali educatori':[ore_totali],'Totale spese':[spese],'Costo tesseramento bimbi':[costo_tesseramenti],'Incasso netto':[incasso_netto]})
+                                          'Ore totali educatori':[ore_totali],'Totale spese':[spese],'Costo tesseramento bimbi':[costo_tesseramenti],'Costo pranzi':[costo_pranzi],'Incasso netto':[incasso_netto]})
                 st.write(df_report)
                 csv = convert_df(df_report)
                 st.download_button(
@@ -418,7 +354,7 @@ def main():
                 #filtra il DataFrame per il giorno selezionato
                 df_giorno = df.loc[df['{}' .format(giorno)] == 'si']
         
-                df_giorno = df_giorno[['Nome','Cognome', 'Eta', 'Classe', 'Pranzo Giorno{}'.format(giorno[-1]),'Foto', 'Diete', 'Allergie']]
+                df_giorno = df_giorno[['Nome','Cognome', 'Eta', 'Classe', 'Pranzo giorno{}'.format(giorno[-1]),'Foto', 'Diete', 'Allergie']]
                 df_giorno_nido_materna = df_giorno.loc[df_giorno['Classe'].isin(['Nido', 'Materna'])]
                 df_giorno_elementari_medie = df_giorno.loc[df_giorno['Classe'].isin(['Elementari', 'Medie'])]
                 #visualizza i dati
@@ -432,7 +368,7 @@ def main():
                 # fig = px.bar(num_bambini_per_classe, x=num_bambini_per_classe.index, y=num_bambini_per_classe.values, labels={'x': 'Classe', 'y':'Numero di bambini'})
                 # st.plotly_chart(fig, use_container_width=True)
                 st.write(num_bambini_per_classe)
-                pranzi=df_giorno['Pranzo Giorno{}'.format(giorno[-1])].value_counts()
+                pranzi=df_giorno['Pranzo giorno{}'.format(giorno[-1])].value_counts()
                 st.write(pranzi)
                 foto_no = df_giorno.loc[df_giorno['Foto'] == 'no']
                 if not foto_no.empty:
--- funzioni.py
+++ funzioni.py
@@ -1,5 +1,71 @@
-def somma_due_numeri(a:float, b:float):
-    '''questa funzione somma due numeri float 
-    e restituisce il loro risultato sommato'''
-    somma = a+b
-    return somma
+import streamlit as st
+import csv
+import pandas as pd
+
+#funzione modifica cella bambini
+def modifica_cella(df, index, colonna, valore):
+    df.at[index, colonna] = valore
+    df.to_csv('dati_bambini.csv', index=False)
+    st.success('Valore modificato!')
+    st.write(df)
+#funzione modifica cella educatori
+def modifica_cella_ed(df, index, colonna, valore):
+    df.at[index, colonna] = valore
+    df.to_csv('educatori.csv', index=False)
+    st.success('Valore modificato!')
+# Funzione per modificare un valore nella tabella delle spese
+def modify_expense_data(row_index, new_value):
+    df = pd.read_csv('spese.csv')
+    df.at[row_index, 'Importo'] = new_value
+    df.to_csv('spese.csv', index=False)
+# Funzione per rimuovere una riga dalla tabella delle spese
+def remove_expense_data(row_index):
+    df = pd.read_csv('spese.csv')
+    df.drop(row_index, inplace=True)
+    df.to_csv('spese.csv', index=False)
+#funzione per scrivere i dati su file (spostata nella pagina Registrazione)
+def scrivi_su_file(nome, cognome, eta, sesso, classe, quota, diete, allergie, email, telefono1, telefono2, indirizzo, cap, comune, giorno1, pranzo1, giorno2, pranzo2, giorno3, pranzo3, giorno4, pranzo4, giorno5, pranzo5, giorno6, pranzo6, giorno7, pranzo7, giorno8, pranzo8, tesseramento, ritiro, parente, foto):
+    #controlla se il bambino è già presente nel file
+    with open('dati_bambini.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['Nome'] == nome and row['Cognome'] == cognome:
+                st.error('Bambino già registrato')
+                return
+    #scrive il nuovo bambino nel file
+    with open('dati_bambini.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([nome, cognome, eta, sesso, classe, quota, diete, allergie, email, telefono1, telefono2, indirizzo, cap, comune, giorno1, pranzo1, giorno2, pranzo2, giorno3, pranzo3, giorno4, pranzo4, giorno5, pranzo5, giorno6, pranzo6, giorno7, pranzo7, giorno8, pranzo8, tesseramento, ritiro, parente, foto])
+
+#funzione per scrivere i dati su file per educatori
+def scrivi_su_file_ed(nome_ed, cognome_ed, ore_giorno1, ore_giorno2, ore_giorno3, ore_giorno4, ore_giorno5, ore_giorno6, ore_giorno7, ore_giorno8):
+    #controlla se l'educatore è già presente nel file
+    with open('educatori.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['Nome'] == nome_ed and row['Cognome'] == cognome_ed:
+                st.error('Educatore già registrato')
+                return
+    
+    #scrive il nuovo educatore nel file
+    with open('educatori.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([nome_ed, cognome_ed, ore_giorno1, ore_giorno2, ore_giorno3, ore_giorno4, ore_giorno5, ore_giorno6, ore_giorno7, ore_giorno8])
+    
+    st.success('Dati salvati!')
+#funzione per scrivere su file spese
+def save_expense_data(tipo_spesa, importo):
+    with open("spese.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([tipo_spesa, importo])
+    st.success("Spesa aggiunta con successo!")
+#funzione per calcolare le ore di ogni educatore
+def calcola_ore_totali(df):
+    df['Ore totali'] = df['Ore giorno1'] + df['Ore giorno2'] + df['Ore giorno3'] + df['Ore giorno4'] + df['Ore giorno5'] + df['Ore giorno6'] + df['Ore giorno7'] + df['Ore giorno8']
+    return df
+#funzione per calcolare la retribuzione di ogni educatore
+def calcola_compensi(df):
+    df['Compensi'] = (df['Ore giorno1'] + df['Ore giorno2'] + df['Ore giorno3'] + df['Ore giorno4']+ df['Ore giorno5'] + df['Ore giorno6'] + df['Ore giorno7'] + df['Ore giorno8'])*9
+    return df
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
